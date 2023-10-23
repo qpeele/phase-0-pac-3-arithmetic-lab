ed026bf386e3cec344d3bae869a746f149959fdd
Quachell Peele <quachellpeele@gmail.com>
2023-10-23 16:02:23 -0400

    struggle

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b) {
+    
+    return a + b;
+
+    console.log(add(2, 3)); // Output: 5
+}
+
+  function subtract(a,b) {
+    return a - b;
+    // we'll fill this in shortly
+  }
+
+  function multiply(a,b) {
+    return a * b;
+    // we'll fill this in shortly
+  }
+
+  function divide(a,b) {
+    return a / b;// we'll fill this in shortly
+  }
+function increment (n){
+
+    return n+=1
+}
+function decrement (n){
+
+    return n-=1
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
